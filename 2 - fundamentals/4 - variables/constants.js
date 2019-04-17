5fd20eda6ed4bbd6151cf4b5494974c2b49d3e55
Luke Hill <lukehill_uk@hotmail.com>
2019-04-17 15:48:17 +0200

    Lessons 1-7 complete

---
+++ 2 - fundamentals/4 - variables/constants.js
@@ -0,0 +1,5 @@
+const birthday = '18.04.1982';
+const age = someCode(birthday);
+
+const BIRTHDAY = '18.04.1982'; // make uppercase? - This never changes; so yes.
+const AGE = someCode(BIRTHDAY); // make uppercase? - This does change, so no.
---
+++ 2 - fundamentals/7 - operators/operators.js
@@ -0,0 +1,7 @@
+let a = 1, b = 1;
+let c = ++a; // 2
+let d = b++; // 1
+
+let a = 2;
+
+let x = 1 + (a *= 2); // 5
